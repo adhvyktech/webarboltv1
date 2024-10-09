2300a06136ee5d853a3e870ba47bcf2e07fceb7d
adhvyktech <adhvyktech@gmail.com>
2024-10-09 13:57:53 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  onError: (error, errorInfo) => {
+    console.log('Global error handler');
+    console.log(error);
+    console.log(errorInfo);
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+    return config
+  }
 };
 
 module.exports = nextConfig;
